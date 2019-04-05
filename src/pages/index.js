e6477716241f26ec63bd6d5e3b967f9111b9ffc6
Harlan Verthein <harlan@verthein.us>
2019-04-04 19:57:15 -0700

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,17 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome to the Gig Worker Aggregator</h1>
+    <p>Introducing a tool to improve your experience as a gig worker</p>
+    <p>Gig Worker Aggregator will help you organize a busy schedule,</p>
+    <p>allowing you to focus on the things that are important</p>
+    <p>rather than making you deal with different apps, payrates, and other headaches</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Click here to find out if GWA is right for you</Link>
+    <p></p>
+    <Link to="/about/">Click here to visit our about page</Link>
   </Layout>
 )
 
