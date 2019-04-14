a4af517705d346978b3d6624d749ad1f03659736
Harlan Verthein <harlan@verthein.us>
2019-04-13 21:03:24 -0700

    Testing adding checkbox

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,16 +4,15 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import ReactDOM from 'react-dom';
+import Application from './components/Application';
+
+
+
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Is GWA the right app for you?</h1>
-    <p>First, think about your answers to the following questions:</p>
-    <p>Do you work multiple jobs between different companies like Uber, Lyft, Amazon Flex, and more</p>
-    <p>Do you want to be compensated as much as possible for your time?</p>
-    <p>Do you struggle with planning your average workday between so many jobs?</p>
-    <p>Would you be interested in receiving benefits for your work at your jobs?</p>
-    <p>Do you find it difficult to aggregate information between your jobs in order to file taxes?</p>
+    ReactDOM.render(<Application />, document.querySelector('[data-react-application]'));
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
