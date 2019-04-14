0394c299b5997a66b8cec390e2f5065df9a164cf
harlan <harlan@verthein.us>
2019-04-13 23:11:36 -0700

    Testing adding checkboxes with labels

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -13,10 +13,12 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <p>Please check all of the companies that you work for:</p>
+    <div>
     <label>
       <Checkbox/>
       Uber
       </label>
+      </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
