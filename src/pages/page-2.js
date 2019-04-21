fde42146982bae541a2bd31e25ca1531003c5b5d
harlan <harlan@verthein.us>
2019-04-21 13:29:44 -0700

    Fixing job list component

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import JobList from "../components/joblist"
+import JobList from "../components/JobList"
 
 
 const SecondPage = () => (
